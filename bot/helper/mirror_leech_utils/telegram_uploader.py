01b6fd989e9d920ee92c33dd56eea6dfea82c335
babupakkakivellu <149465835+babupakkakivellu@users.noreply.github.com>
2024-06-12 09:25:13 +0530

    Update telegram_uploader.py

--- bot/helper/mirror_leech_utils/telegram_uploader.py
+++ bot/helper/mirror_leech_utils/telegram_uploader.py
@@ -130,7 +130,7 @@ class TgUploader:
 
     async def _prepare_file(self, file_, dirpath, delete_file):
         if self._lprefix:
-            cap_mono = f"{self._lprefix} <code>{file_}</code>"
+            cap_mono = f"{self._lprefix} <b>{file_}</b>"
             self._lprefix = re_sub("<.*?>", "", self._lprefix)
             if (
                 self._listener.seed
@@ -147,7 +147,7 @@ class TgUploader:
                 await rename(self._up_path, new_path)
                 self._up_path = new_path
         else:
-            cap_mono = f"<code>{file_}</code>"
+            cap_mono = f"<b>{file_}</b>"
         if len(file_) > 60:
             if is_archive(file_):
                 name = get_base_name(file_)
